5ad0b587f2e0f173c23fd9238b7073f7939c60f1
Cocotterooo <fcocoterooo@gmail.com>
2024-12-07 16:31:05 +0100

    Refactorización de código para legibilidad

--- bot.py
+++ bot.py
@@ -1,54 +1,71 @@
+import os
+import asyncio
 import discord
 from discord import app_commands
 from dotenv import load_dotenv
-import os
-import asyncio
-import io
 
-# Mensajes de Instagram:
+# Utilidades y módulos personalizados
 from utils.welcome_image import generate_welcome_image
-from utils.api.instagram.Instagram import InstagramAPI 
+from utils.api.instagram.Instagram import InstagramAPI
 from utils.db.authentication import supabase_autenticated
-from utils.periodic_tasks.renew import renew_all_likes_comments_task, renew_media_url_task, check_new_post_task
+from utils.periodic_tasks.renew import (
+    renew_all_likes_comments_task,
+    renew_media_url_task,
+    check_new_post_task,
+)
 from utils.db.Posts import Post
 from utils.db.Discord_instagram_messsage import Dc_insta_msg
 from utils.interactions.instagram_commands import instagram_send_command
-
-# Selector de Roles de la DAI:
 from utils.interactions.dai_roles import dai_roles_interaction, dai_roles
-
-# Creador de canales de voz:
-from utils.interactions.interactive_voice_channel import voice_channel_creator, create_or_update_channel, monitor_channel_activity, resume_channel_monitoring
-from config import voice_channel_creator_embed
-
-# Encuestas en canales de voz:
+from utils.interactions.interactive_voice_channel import (
+    voice_channel_creator,
+    create_or_update_channel,
+    monitor_channel_activity,
+    resume_channel_monitoring,
+)
 from utils.interactions.private_poll import voice_poll_cmd
-
-# Soporte y verificación:
-from utils.interactions.support_and_verification import support_and_verification, handle_ticket_interaction
-
-# Comandos de Asociaciones
-from utils.interactions.asociations import nuevo_spacelab, eliminar_spacelab, nuevo_motorsport, eliminar_motorsport, nuevo_ces, eliminar_ces, setup_spacelab, setup_motorsport, setup_ces, nuevo_ceeibis, eliminar_ceeibis, setup_ceeibis
-
-# Constantes
-from config import SERVER_ID, LOG_CHANNEL, WELCOME_CHANNEL, INSTAGRAM_DAI_CHANNEL, ADMIN_ROLE, linktree_embed
+from utils.interactions.support_and_verification import (
+    support_and_verification,
+    handle_ticket_interaction,
+)
+from utils.interactions.asociations import (
+    nuevo_spacelab,
+    eliminar_spacelab,
+    nuevo_motorsport,
+    eliminar_motorsport,
+    nuevo_ces,
+    eliminar_ces,
+    setup_spacelab,
+    setup_motorsport,
+    setup_ces,
+    nuevo_ceeibis,
+    eliminar_ceeibis,
+    setup_ceeibis,
+)
+from config import (
+    SERVER_ID,
+    LOG_CHANNEL,
+    WELCOME_CHANNEL,
+    INSTAGRAM_DAI_CHANNEL,
+    ADMIN_ROLE,
+    linktree_embed,
+    voice_channel_creator_embed,
+)
 
 # Cargar el archivo .env
 load_dotenv()
 
-#! Crear una instancia del bot
-TOKEN = os.getenv('DISCORD_TOKEN')
+# Crear instancias necesarias
+TOKEN = os.getenv("DISCORD_TOKEN")
 MY_GUILD = discord.Object(id=SERVER_ID)
 
-#! Crear una instancia de Supabase
-DB_URL: str = os.environ.get("DB_URL")
-DB_API_KEY: str = os.environ.get("DB_API_KEY")
-DB_EMAIL: str = os.environ.get("DB_EMAIL")
-DB_EMAIL_PASSWORD: str = os.environ.get("DB_EMAIL_PASSWORD")
+DB_URL = os.getenv("DB_URL")
+DB_API_KEY = os.getenv("DB_API_KEY")
+DB_EMAIL = os.getenv("DB_EMAIL")
+DB_EMAIL_PASSWORD = os.getenv("DB_EMAIL_PASSWORD")
 supabase = supabase_autenticated(DB_URL, DB_API_KEY, DB_EMAIL, DB_EMAIL_PASSWORD)
 
-#! Crear una instancia de Instagram
-INSTAGRAM_API_KEY = os.getenv('INSTAGRAM_API_KEY')
+INSTAGRAM_API_KEY = os.getenv("INSTAGRAM_API_KEY")
 instagram = InstagramAPI(INSTAGRAM_API_KEY)
 posts = Post(supabase, instagram)
 
@@ -58,10 +75,11 @@ class Bot(discord.Client):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
 
-    #* Aquí sincronizamos los comandos de aplicación con el servidor especificado (MY_GUILD).
     async def setup_hook(self):
         dai_roles(self)
-        instagram_send_command(self, supabase, instagram, dc_insta_msg, INSTAGRAM_DAI_CHANNEL, ADMIN_ROLE)
+        instagram_send_command(
+            self, supabase, instagram, dc_insta_msg, INSTAGRAM_DAI_CHANNEL, ADMIN_ROLE
+        )
         voice_channel_creator(self, ADMIN_ROLE, voice_channel_creator_embed())
         voice_poll_cmd(self)
         support_and_verification(self)
@@ -77,89 +95,97 @@ class Bot(discord.Client):
         setup_motorsport(self)
         setup_ces(self)
         setup_ceeibis(self)
-        # Copiamos los comandos globales a nuestro servidor 
-        # Esto evita tener que esperar la propagación global de hasta una hora.
         self.tree.copy_global_to(guild=MY_GUILD)
-        # Sincronizamos los comandos en el árbol para asegurarnos de que estén listos en el servidor.
         await self.tree.sync(guild=MY_GUILD)
 
-# Las intenciones definen los eventos a los que el bot estará atento, como mensajes, miembros, etc.
+
 intents = discord.Intents.default()
-# Habilita la intención de recibir eventos relacionados con miembros, mensajes y servidores.
-intents.members = True  
-intents.messages = True  
-intents.guilds = True  
+intents.members = True
+intents.messages = True
+intents.guilds = True
 client = Bot(intents=intents)
 
-
-#! Crear una instancia de Discord_instagram_messsage
 dc_insta_msg = Dc_insta_msg(supabase, client, instagram)
 
 
 @client.event
 async def on_ready():
-    print(f'Bot conectado como {client.user}')
-    #log_channel = client.get_channel(LOG_CHANNEL) 
-    #await log_channel.send('Estado Bot: **Online** <a:online:1288631919352877097>')
+    print(f"Bot conectado como {client.user}")
     await resume_channel_monitoring(client, supabase)
-    asyncio.create_task(renew_all_likes_comments_task(posts, 3600, client, supabase, INSTAGRAM_DAI_CHANNEL))
+    asyncio.create_task(
+        renew_all_likes_comments_task(posts, 3600, client, supabase, INSTAGRAM_DAI_CHANNEL)
+    )
     asyncio.create_task(renew_media_url_task(posts, 86400))
     asyncio.create_task(check_new_post_task(instagram, posts, dc_insta_msg, 3600))
 
 
 @client.event
-async def on_interaction(interaction):
+async def on_interaction(interaction: discord.Interaction):
     await dai_roles_interaction(interaction)
 
-    # Verificar si la interacción contiene un "custom_id"
     if interaction.data and interaction.data.get("custom_id"):
         custom_id = interaction.data["custom_id"]
-        # Verifica si la interacción es un botón del sistema de ticket
-        ticket_related_ids = ["create_ticket", "verify", "close_ticket", "deny_verification", "accept_verification"]
+        ticket_related_ids = [
+            "create_ticket",
+            "verify",
+            "close_ticket",
+            "deny_verification",
+            "accept_verification",
+        ]
         if custom_id in ticket_related_ids:
             await handle_ticket_interaction(interaction)
-        # Si no es un botón de ticket, proceder con el creador de canales
         await create_or_update_channel(client, supabase, interaction)
 
 
-# Evento cuando un miembro se une al servidor
 @client.event
-async def on_member_join(member):
-    print(f'{member} se ha unido al servidor')
-    # Obtener el avatar y el nombre del miembro
+async def on_member_join(member: discord.Member):
+    print(f"{member} se ha unido al servidor")
     avatar_url = member.avatar.url if member.avatar else member.default_avatar.url
     nombre_usuario = member.name
-    # Envia la imagen
-    channel = client.get_channel(WELCOME_CHANNEL) 
-    image_binary = generate_welcome_image(nombre_usuario, avatar_url, 'assets/tema_claro_recortado.png')
+
+    channel = client.get_channel(WELCOME_CHANNEL)
+    image_binary = generate_welcome_image(
+        nombre_usuario, avatar_url, "assets/tema_claro_recortado.png"
+    )
     if image_binary is None:
-        await print(f'Ocurrió un error al generar la imagen de Bienvenida de {nombre_usuario}')
+        print(f"Ocurrió un error al generar la imagen de Bienvenida de {nombre_usuario}")
     else:
-        await channel.send(file=discord.File(fp=image_binary, filename='bienvenida.png'))
-        await channel.send(f'<:entrar:1288631392070012960> {member.mention} ¡Bienvenid@ a la **Comunidad Oficial** de la **EEI**! 🎉 \n> <:verificado:1288628715982553188> Si eres estudiante en la **EEI verifica tu cuenta** en <#1299775062215229460>\n-#       **Delegación de Alumnos** EEI - Uvigo')
+        await channel.send(file=discord.File(fp=image_binary, filename="bienvenida.png"))
+        await channel.send(
+            f"<:entrar:1288631392070012960> {member.mention} ¡Bienvenid@ a la "
+            "**Comunidad Oficial** de la **EEI**! 🎉 \n"
+            "> <:verificado:1288628715982553188> Si eres estudiante en la **EEI verifica tu cuenta** en "
+            "<#1299775062215229460>\n-#       **Delegación de Alumnos** EEI - Uvigo"
+        )
 
 
-@client.tree.command(name='bienvenida', description='Imagen de Bienvenida')
+@client.tree.command(name="bienvenida", description="Imagen de Bienvenida")
 @app_commands.checks.has_role(ADMIN_ROLE)
 async def welcome(interaction: discord.Interaction):
     avatar_url = interaction.user.avatar.url
     nombre_usuario = interaction.user.name
-    image_binary = generate_welcome_image(nombre_usuario, avatar_url, 'assets/tema_claro_recortado.png')
-    # Envia la imagen
-    channel = client.get_channel(WELCOME_CHANNEL) 
-    image_binary = generate_welcome_image(nombre_usuario, avatar_url, 'assets/tema_claro_recortado.png')
+
+    channel = client.get_channel(WELCOME_CHANNEL)
+    image_binary = generate_welcome_image(
+        nombre_usuario, avatar_url, "assets/tema_claro_recortado.png"
+    )
     if image_binary is None:
-        await print(f'Ocurrió un error al generar la imagen de Bienvenida de {nombre_usuario}')
+        print(f"Ocurrió un error al generar la imagen de Bienvenida de {nombre_usuario}")
     else:
-        await channel.send(file=discord.File(fp=image_binary, filename='bienvenida.png'))
-        await channel.send(f'<:entrar:1288631392070012960>  ¡Bienvenid@ a la **Comunidad Oficial** de la **EEI**! 🎉\n-#       **Delegación de Alumnos** EEI - Uvigo')
+        await channel.send(file=discord.File(fp=image_binary, filename="bienvenida.png"))
+        await channel.send(
+            f"<:entrar:1288631392070012960> ¡Bienvenid@ a la **Comunidad Oficial** de la "
+            "**EEI**! 🎉\n-#       **Delegación de Alumnos** EEI - Uvigo"
+        )
 
 
-@client.tree.command(name='rss', description='Muestra las redes sociales y la web de la Delegación de Alumnos de Industriales')
+@client.tree.command(
+    name="rss",
+    description="Muestra las redes sociales y la web de la Delegación de Alumnos de Industriales",
+)
 async def web(interaction: discord.Interaction):
-    await interaction.response.defer(thinking=True)  # Indica que se está procesando
+    await interaction.response.defer(thinking=True)
     await interaction.followup.send(embed=linktree_embed(), ephemeral=False)
-    return
 
 
 client.run(TOKEN)
--- utils/interactions/support_and_verification.py
+++ utils/interactions/support_and_verification.py
@@ -2,98 +2,188 @@ import discord
 from discord.ext import commands
 from discord import ButtonStyle, Interaction, ui, app_commands
 
-from config import ADMIN_ROLE, TICKET_CATEGORY_ID, VERIFICATION_CATEGORY_ID, DAI_MEMBER_ROLE_ID, VERIFIED_ROLE_ID, tickets_embed, verification_embed, support_and_verification_embed
+from config import (
+    ADMIN_ROLE,
+    TICKET_CATEGORY_ID,
+    VERIFICATION_CATEGORY_ID,
+    DAI_MEMBER_ROLE_ID,
+    VERIFIED_ROLE_ID,
+    tickets_embed,
+    verification_embed,
+    support_and_verification_embed,
+)
+
 
 class TicketView(ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-        self.add_item(discord.ui.Button(label="📩 Crea un Ticket", style=ButtonStyle.blurple, custom_id="create_ticket"))
-        self.add_item(discord.ui.Button(label="☑️ Verifícate", style=ButtonStyle.green, custom_id="verify"))
+        self.add_item(
+            discord.ui.Button(
+                label="📩 Crea un Ticket",
+                style=ButtonStyle.blurple,
+                custom_id="create_ticket",
+            )
+        )
+        self.add_item(
+            discord.ui.Button(
+                label="☑️ Verifícate",
+                style=ButtonStyle.green,
+                custom_id="verify",
+            )
+        )
+
 
 class CloseTicketView(ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-        self.add_item(discord.ui.Button(label="Cerrar Ticket", style=ButtonStyle.red, custom_id="close_ticket"))
+        self.add_item(
+            discord.ui.Button(
+                label="Cerrar Ticket",
+                style=ButtonStyle.red,
+                custom_id="close_ticket",
+            )
+        )
+
 
 class VerificationView(ui.View):
     def __init__(self, user: discord.User):
         super().__init__(timeout=None)
-        self.user = user  # Guardamos al usuario que creó el canal
-        self.add_item(discord.ui.Button(label="No Verificar", style=ButtonStyle.red, custom_id="deny_verification"))
-        self.add_item(discord.ui.Button(label="Verificar", style=ButtonStyle.green, custom_id="accept_verification"))
+        self.user = user
+        self.add_item(
+            discord.ui.Button(
+                label="No Verificar",
+                style=ButtonStyle.red,
+                custom_id="deny_verification",
+            )
+        )
+        self.add_item(
+            discord.ui.Button(
+                label="Verificar",
+                style=ButtonStyle.green,
+                custom_id="accept_verification",
+            )
+        )
+
 
 async def create_ticket(interaction: discord.Interaction):
     guild = interaction.guild
-    existing_ticket_channel = discord.utils.get(guild.channels, name=f"🎫⦙{interaction.user.name}")
+    existing_ticket_channel = discord.utils.get(
+        guild.channels, name=f"🎫⦙{interaction.user.name}"
+    )
     if existing_ticket_channel:
-        return await interaction.response.send_message("<:no:1288631410558767156> Ya tienes un ticket abierto.", ephemeral=True)
+        return await interaction.response.send_message(
+            "<:no:1288631410558767156> Ya tienes un ticket abierto.", ephemeral=True
+        )
 
     category = guild.get_channel(TICKET_CATEGORY_ID)
-    ticket_channel = await category.create_text_channel(name=f"🎫⦙{interaction.user.name}")
+    ticket_channel = await category.create_text_channel(
+        name=f"🎫⦙{interaction.user.name}"
+    )
 
-    await ticket_channel.set_permissions(guild.default_role, read_messages=False)
-    await ticket_channel.set_permissions(interaction.user, read_messages=True, send_messages=True, attach_files=True)
-    await ticket_channel.set_permissions(guild.get_role(DAI_MEMBER_ROLE_ID), read_messages=True, send_messages=True, attach_files=True)
+    await ticket_channel.set_permissions(
+        guild.default_role, read_messages=False
+    )
+    await ticket_channel.set_permissions(
+        interaction.user, read_messages=True, send_messages=True, attach_files=True
+    )
+    await ticket_channel.set_permissions(
+        guild.get_role(DAI_MEMBER_ROLE_ID),
+        read_messages=True,
+        send_messages=True,
+        attach_files=True,
+    )
 
     embed = tickets_embed(interaction.user)
     await ticket_channel.send(interaction.user.mention, embed=embed, view=CloseTicketView())
 
-    await interaction.response.send_message(f"<:correcto:1288631406452412428> Ticket creado: {ticket_channel.mention}", ephemeral=True)
+    await interaction.response.send_message(
+        f"<:correcto:1288631406452412428> Ticket creado: {ticket_channel.mention}",
+        ephemeral=True,
+    )
+
 
 async def verify(interaction: discord.Interaction):
     guild = interaction.guild
-    existing_ticket_channel = discord.utils.get(guild.channels, name=f"✅⦙{interaction.user.name}")
-    # Verificar que el usuario aun no tenga el rol verificado
+    existing_ticket_channel = discord.utils.get(
+        guild.channels, name=f"✅⦙{interaction.user.name}"
+    )
     verified_role = guild.get_role(VERIFIED_ROLE_ID)
     if verified_role in interaction.user.roles:
-        return await interaction.response.send_message("<:no:1288631410558767156> Ya estás verificado.", ephemeral=True)
-    # Verificar si el usuario ya tiene un ticket de verificación abierto
+        return await interaction.response.send_message(
+            "<:no:1288631410558767156> Ya estás verificado.", ephemeral=True
+        )
     if existing_ticket_channel:
-        return await interaction.response.send_message("<:no:1288631410558767156> Ya tienes un ticket de verificación abierto.", ephemeral=True)
+        return await interaction.response.send_message(
+            "<:no:1288631410558767156> Ya tienes un ticket de verificación abierto.",
+            ephemeral=True,
+        )
 
     category = guild.get_channel(VERIFICATION_CATEGORY_ID)
-    verify_channel = await category.create_text_channel(name=f"✅⦙{interaction.user.name}")
+    verify_channel = await category.create_text_channel(
+        name=f"✅⦙{interaction.user.name}"
+    )
 
-    await verify_channel.set_permissions(guild.default_role, read_messages=False)
-    await verify_channel.set_permissions(interaction.user, read_messages=True, send_messages=True, attach_files=True)
-    await verify_channel.set_permissions(guild.get_role(DAI_MEMBER_ROLE_ID), read_messages=True, send_messages=True, attach_files=True)
+    await verify_channel.set_permissions(
+        guild.default_role, read_messages=False
+    )
+    await verify_channel.set_permissions(
+        interaction.user, read_messages=True, send_messages=True, attach_files=True
+    )
+    await verify_channel.set_permissions(
+        guild.get_role(DAI_MEMBER_ROLE_ID),
+        read_messages=True,
+        send_messages=True,
+        attach_files=True,
+    )
 
     embed = verification_embed(interaction.user)
-    await verify_channel.send(interaction.user.mention, embed=embed, view=VerificationView(interaction.user))
+    await verify_channel.send(interaction.user.mention, 
+                              embed=embed, 
+                              view=VerificationView(interaction.user)
+    )
 
-    await interaction.response.send_message(f"<:correcto:1288631406452412428> Canal de verificación creado: {verify_channel.mention}", ephemeral=True)
+    await interaction.response.send_message(
+        f"<:correcto:1288631406452412428> Canal de verificación creado: {verify_channel.mention}",
+        ephemeral=True,
+    )
+
 
 async def close_ticket(interaction: discord.Interaction):
     await interaction.channel.delete()
+
 
 async def deny_verification(interaction: discord.Interaction):
     guild = interaction.guild
     support_role = guild.get_role(DAI_MEMBER_ROLE_ID)
     if support_role not in interaction.user.roles:
-        return await interaction.response.send_message("<:no:1288631410558767156> No tienes permiso para usar este botón.", ephemeral=True)
-
+        return await interaction.response.send_message(
+            "<:no:1288631410558767156> No tienes permiso para usar este botón.", ephemeral=True
+        )
     await interaction.channel.delete()
+
 
 async def accept_verification(interaction: discord.Interaction):
     guild = interaction.guild
     support_role = guild.get_role(DAI_MEMBER_ROLE_ID)
     if support_role not in interaction.user.roles:
-        return await interaction.response.send_message("<:no:1288631410558767156> No tienes permiso para usar este botón.", ephemeral=True)
+        return await interaction.response.send_message(
+            "<:no:1288631410558767156> No tienes permiso para usar este botón.", ephemeral=True
+        )
 
-    # Extraer el nombre del usuario del canal
     user_name = interaction.channel.name.replace("✅⦙", "")
-    
-    # Buscar al usuario en el servidor
     member = discord.utils.get(guild.members, name=user_name)
     if member is None:
-        return await interaction.response.send_message("<:no:1288631410558767156> No se encontró al usuario correspondiente.", ephemeral=True)
+        return await interaction.response.send_message(
+            "<:no:1288631410558767156> No se encontró al usuario correspondiente.",
+            ephemeral=True,
+        )
 
     verified_role = guild.get_role(VERIFIED_ROLE_ID)
-    await member.add_roles(verified_role)  # Asignar el rol al usuario encontrado
+    await member.add_roles(verified_role)
     await interaction.channel.delete()
 
+
 async def handle_ticket_interaction(interaction: discord.Interaction):
-    """Encapsula el manejo de interacciones relacionadas con tickets."""
     if interaction.data and interaction.data.get("custom_id"):
         custom_id = interaction.data.get("custom_id")
         if custom_id == "create_ticket":
@@ -107,18 +197,26 @@ async def handle_ticket_interaction(interaction: discord.Interaction):
         elif custom_id == "accept_verification":
             await accept_verification(interaction)
 
+
 def support_and_verification(bot):
-    @bot.tree.command(name="setup_soporte_verificacion", description="Enviar el embed principal")
-    @app_commands.describe(channel='La ID del chat donde se enviará el embed')
+    @bot.tree.command(
+        name="setup_soporte_verificacion", description="Enviar el embed principal"
+    )
+    @app_commands.describe(channel="La ID del chat donde se enviará el embed")
     @app_commands.checks.has_role(ADMIN_ROLE)
     async def send_main_embed(interaction: discord.Interaction, channel: discord.TextChannel):
-        """Slash Command que envía el embed principal al canal seleccionado"""
         embed = support_and_verification_embed()
         view = TicketView()
         await channel.send(embed=embed, view=view)
-        await interaction.response.send_message(f"<:correcto:1288631406452412428> Embed enviado a {channel.mention}", ephemeral=True)
+        await interaction.response.send_message(
+            f"<:correcto:1288631406452412428> Embed enviado a {channel.mention}",
+            ephemeral=True,
+        )
 
     @send_main_embed.error
     async def send_main_embed_error(interaction: discord.Interaction, error):
         if isinstance(error, app_commands.MissingRole):
-            await interaction.response.send_message("<:no:1288631410558767156> No tienes permisos para usar este comando.", ephemeral=True)
+            await interaction.response.send_message(
+                "<:no:1288631410558767156> No tienes permisos para usar este comando.",
+                ephemeral=True,
+            )
